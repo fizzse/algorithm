1b3c19dc20950f61d8f467916b0318e4c0b84c53
ssong <ssong@arxanculture.com>
2019-02-28 10:54:15 +0800

    修改快排

--- quick_sort.go
+++ quick_sort.go
@@ -26,12 +26,16 @@ func SeparateSort(arr []int, start, end int) {
 }
 
 func Partition(arr []int, start, end int) int {
+	// 取最后一个元素为判断数据
+	// 保证右边的都比自己小
+	// 左边(没有元素),都比自己大
 	pivot := arr[end]
 
 	i := start
 	for j := start; j < end; j++ {
+		// 如果右边的数据小,则哨兵后移
 		if arr[j] < pivot {
-			if i == j {
+			if i != j {
 				arr[i], arr[j] = arr[j], arr[i]
 			}
 			i++
@@ -43,7 +47,7 @@ func Partition(arr []int, start, end int) int {
 }
 
 func main() {
-	arr := []int{9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+	arr := []int{1, 3, 5, 7, 9, 2, 4, 6, 8, 0}
 	QuickSort(arr)
 	fmt.Println(arr)
 }
